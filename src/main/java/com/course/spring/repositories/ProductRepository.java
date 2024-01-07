94275ee713ff7ee162ebbad4549fe02519630102
SamaraMoreira <128256951+SamaraMoreira@users.noreply.github.com>
2024-01-07 16:56:05 -0300

    Product

---
+++ src/main/java/com/course/spring/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.course.spring.repositories;
+
+import com.course.spring.entities.Order;
+import com.course.spring.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
